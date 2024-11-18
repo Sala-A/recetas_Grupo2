4865ebceb649e0124cc463680c12b15a4c163c75
Aldenet <aldenet2@gmail.com>
2024-11-17 22:02:04 -0500

    Update permissions.py
    permisos agregados

--- receta/calificaciones/api/permissions.py
+++ receta/calificaciones/api/permissions.py
@@ -2,7 +2,8 @@ from rest_framework.permissions import BasePermission
 
 class IsAuthenticatedToCreate(BasePermission):
     def has_permission(self, request, view):
-        if request.method == 'GET':
+from rest_framework.permissions import BasePermission
+        if request.method == 'GET'or request.method=='POST' or request.method=='DELETE' or request.method=='PUT':
             return True
         elif request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
             return request.user.is_authenticated
